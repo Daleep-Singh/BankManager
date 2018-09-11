cf3b46466310ef799e30a902c2c554714f4ca208
Daleep Singh <daleep.singh@mail.utoronto.ca>
2018-09-11 15:23:45 -0400

    First Creation of Programs

---
+++ Manager.java
@@ -0,0 +1,17 @@
+import java.util.Scanner
+public class Manager{
+	
+	public static void main(string[] args){
+		Scanner input = new Scanner(input.in);
+		System.out.println("Do You Want to create a bank account? (yes/no)");
+		String s = input.next;
+		if (s == 'no'){
+			System.out.println("okay bye");
+			System.Exit()
+		} else if (s == 'yes'){
+			System.out.println("enter username")
+		
+		
+		
+	}
+}
